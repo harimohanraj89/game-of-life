ab851a2ac5c6d8d8623e2cf743a48eca9ee6801e
harimohanraj89 <hari.mohanraj89@gmail.com>
2014-11-25 15:51:40 -0500

    cell and tests

---
+++ js/cell.js
@@ -0,0 +1,35 @@
+function Cell() {
+  this.alive = false;
+  this.neighbors = [];
+}
+
+Cell.prototype.setLife = function(lifeState) {
+  this.alive = lifeState;
+};
+
+Cell.prototype.spawn = function() {
+  this.setLife(true);
+};
+
+Cell.prototype.kill = function() {
+  this.setLife(false);
+};
+
+Cell.prototype.setNeighbors = function(neighbors) {
+  this.neighbors = neighbors;
+};
+
+Cell.prototype.livingNeighbors = function() {
+  return this.neighbors.reduce(function(count, neighbor) {
+    // Add one to count if neighbor is alive
+    return neighbor.alive ? count + 1 : count;
+  }, 0);
+};
+
+Cell.prototype.shouldLive = function() {
+  if (this.livingNeighbors() === 2 || this.livingNeighbors() === 3) {
+    return true;
+  } else {
+    return false;
+  }
+};
---
+++ spec/spec/CellSpec.js
@@ -0,0 +1,88 @@
+describe("Cell", function() {
+  var player;
+  var song;
+
+  beforeEach(function() {
+    cell = new Cell();
+  });
+
+  it("should start off dead", function() {
+    expect(cell.alive).toBe(false);
+  });
+
+  describe("#setLife", function() {
+    it("should be able to set the cell to live", function() {
+      cell.setLife(true);
+      expect(cell.alive).toBe(true);
+    });
+
+    it("should be able to set the cell to die", function() {
+      cell.setLife(false);
+      expect(cell.alive).toBe(false);
+    });
+  });
+
+  describe("#spawn", function() {
+    it("should set the cell to live", function() {
+      cell.spawn();
+      expect(cell.alive).toBe(true);
+    });
+  });
+
+  describe("#set", function() {
+    it("should be able to set the cell to live", function() {
+      cell.setLife(true);
+      cell.kill();
+      expect(cell.alive).toBe(false);
+    });
+  });
+
+  describe("#livingNeighbors", function() {
+    it("should return the number of living neighbors around a cell", function() {
+      neighbor1 = new Cell();
+      neighbor2 = new Cell();
+      neighbor3 = new Cell();
+      neighbor2.spawn();
+      neighbor3.spawn();
+      cell.setNeighbors([neighbor1, neighbor2, neighbor3]);
+      expect(cell.livingNeighbors()).toBe(2);
+    });
+  });
+
+  describe("#shouldLive", function() {
+    it("should return true for 2 neighbors", function() {
+      neighbors = [new Cell(), new Cell(), new Cell(), new Cell(), new Cell(), new Cell()];
+      neighbors[0].spawn();
+      neighbors[2].spawn();
+      cell.setNeighbors(neighbors);
+      expect(cell.shouldLive()).toBe(true);
+    });
+
+    it("should return true for 3 neighbors", function() {
+      neighbors = [new Cell(), new Cell(), new Cell(), new Cell(), new Cell(), new Cell()];
+      neighbors[1].spawn();
+      neighbors[3].spawn();
+      neighbors[5].spawn();
+      cell.setNeighbors(neighbors);
+      expect(cell.shouldLive()).toBe(true);
+    });
+
+    it("should return false for less than 2 neighbors", function() {
+      neighbors = [new Cell(), new Cell(), new Cell(), new Cell(), new Cell(), new Cell()];
+      neighbors[4].spawn();
+      cell.setNeighbors(neighbors);
+      expect(cell.shouldLive()).toBe(false);
+    });
+
+    it("should return false for more than 3 neighbors", function() {
+      neighbors = [new Cell(), new Cell(), new Cell(), new Cell(), new Cell(), new Cell()];
+      neighbors[1].spawn();
+      neighbors[2].spawn();
+      neighbors[3].spawn();
+      neighbors[4].spawn();
+      neighbors[5].spawn();
+      cell.setNeighbors(neighbors);
+      expect(cell.shouldLive()).toBe(false);
+    });
+  });
+});
